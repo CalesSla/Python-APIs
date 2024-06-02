8fbe3b7183c9b99e45ef47adde532e762ffc6951
Slava <veaceslavcalestru@gmail.com>
2024-06-02 10:44:33 +0300

    update post method

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+import requests
 
 
 
@@ -25,6 +26,11 @@ def find_post(id):
     for p in my_posts:
         if p['id'] == id:
             return p
+        
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 @app.get("/")
 def root():
@@ -34,7 +40,7 @@ def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict["id"] = randrange(0, 1000000)
@@ -46,8 +52,35 @@ def add(added: Addition):
     return{"result": added.a + added.b}
 
 @app.get("/posts/{id}")
-def get_post(id):
-    post = find_post(int(id))
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     return {"post_detail": post}
 
 
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+
+    index = find_index_post(id)
+    base_url = "http://127.0.0.1:8000"
+    post_id = id
+
+    response = dict(requests.get(f"{base_url}/posts/{post_id}").json()['post_detail'])
+    print(f"Existing post: {response}")
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+    post_dict = post.dict()
+    post_dict["id"] = id
+    my_posts[index] = post_dict
+    print(f"New post: {my_posts[index]}")
+    return {"data": post_dict}

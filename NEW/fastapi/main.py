6f0290d8f3804b9e6f67cf3105ea13bac62ae335
Slava Calestru <veaceslavcalestru@gmail.com>
2024-12-15 00:05:57 +0000

    linreg model trained and post handle created

--- NEW/fastapi/main.py
+++ NEW/fastapi/main.py
@@ -1,8 +1,43 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+import pickle
+import numpy as np
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+
+class FeatureData(BaseModel):
+    x: list[float]
+
+
 @app.get("/")
-def get_user():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Hello World!!!!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    
+    return {"data": new_post}
+
+
+@app.post("/predict")
+def predict(data: FeatureData):
+    features = np.array([data.x]).reshape(-1,1)
+    model_path = "trainedModels/trained_linreg_model.pkl"
+    with open(model_path, 'rb') as file:
+        loaded_model = pickle.load(file)
+    prediction = loaded_model.predict(features)
+    prediction = prediction.flatten().tolist()
+    return {"prediction": prediction}
+
 

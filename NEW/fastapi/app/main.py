f3c3c22cda2a07b33a039e99253d56d160908c1a
Slava Calestru <veaceslavcalestru@gmail.com>
2024-12-21 14:32:00 +0000

    devine users table and schema

--- NEW/fastapi/app/main.py
+++ NEW/fastapi/app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
-from pydantic import BaseModel
 import pickle
 import numpy as np
-from typing import Optional
+from typing import Optional, List
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
@@ -13,22 +12,13 @@ import os
 import time
 from sqlalchemy.orm import Session
 from sqlalchemy.dialects.postgresql import insert
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
-class FeatureData(BaseModel):
-    x: list[float]
 
 
 while True:
@@ -74,24 +64,18 @@ def root():
     return {"message": "Hello World!!!!"}
 
 
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return {"data": posts}
-
-
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * 
     #                     FROM posts
     #                     """)
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""
     #                 INSERT INTO posts
     #                 (title, content, published)
@@ -105,36 +89,10 @@ def create_posts(post: Post, db: Session = Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data": new_post}
+    return new_post
 
 
-@app.post("/predict", status_code=status.HTTP_201_CREATED)
-def predict(data: FeatureData, db: Session = Depends(get_db)):
-    data = data.dict()
-    features = np.array([data["x"]]).reshape(-1,1)
-    model_path = "app/trainedModels/trained_linreg_model.pkl"
-    with open(model_path, 'rb') as file:
-        loaded_model = pickle.load(file)
-    prediction = loaded_model.predict(features)
-    prediction = prediction.flatten().tolist()
-    for i in range(features.shape[0]):
-    #     cursor.execute("""
-    #                     INSERT INTO predictions
-    #                     (x, y)
-    #                     VALUES (%s, %s)
-    #                     ON CONFLICT (x) DO NOTHING
-    #                     RETURNING *
-    #                         """,
-    #                     (int(features[i][0]), float(prediction[i])))
-        stmt = insert(models.Predictions).values(x=int(features[i][0]), y=float(prediction[i]))
-        stmt = stmt.on_conflict_do_nothing(index_elements=["x"])
-        db.execute(stmt)
-    # conn.commit()
-    db.commit()
-    return {"prediction": prediction}
-
-
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute("""
     #                 SELECT *
@@ -147,7 +105,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-    return {"post_detail": post}
+    return post
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -171,20 +129,97 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    cursor.execute("""
-                   UPDATE posts
-                   SET title = %s,
-                   content = %s,
-                   published = %s
-                   WHERE id = %s
-                   RETURNING *
-                   """, (post.title, post.content, post.published, str(id)))
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+    # cursor.execute("""
+    #                UPDATE posts
+    #                SET title = %s,
+    #                content = %s,
+    #                published = %s
+    #                WHERE id = %s
+    #                RETURNING *
+    #                """, (post.title, post.content, post.published, str(id)))
     
-    updated_post = cursor.fetchone()
-    conn.commit()
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
     
-    if updated_post == None:
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
-    return {"data": updated_post}
+    
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    
+    return post_query.first()
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session=Depends(get_db)):
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+
+
+@app.get("/predict", response_model=List[schemas.Prediction])
+def get_predictions(db: Session = Depends(get_db)):
+    predictions = db.query(models.Predictions).all()
+    return predictions
+
+
+@app.post("/predict", status_code=status.HTTP_201_CREATED, response_model=schemas.PredictionsList)
+def predict(data: schemas.Feature, db: Session = Depends(get_db)):
+    data = data.dict()
+    features = np.array([data["x"]]).reshape(-1,1)
+    model_path = "app/trainedModels/trained_linreg_model.pkl"
+    with open(model_path, 'rb') as file:
+        loaded_model = pickle.load(file)
+    prediction = loaded_model.predict(features)
+    prediction = prediction.flatten().tolist()
+    for i in range(features.shape[0]):
+    #     cursor.execute("""
+    #                     INSERT INTO predictions
+    #                     (x, y)
+    #                     VALUES (%s, %s)
+    #                     ON CONFLICT (x) DO NOTHING
+    #                     RETURNING *
+    #                         """,
+    #                     (int(features[i][0]), float(prediction[i])))
+        stmt = insert(models.Predictions).values(x=int(features[i][0]), y=float(prediction[i]))
+        stmt = stmt.on_conflict_do_nothing(index_elements=["x"])
+        db.execute(stmt)
+    # conn.commit()
+    db.commit()
+    prediction = schemas.PredictionsList(prediction)
+    return prediction
+
+
+@app.delete("/predict/{x}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_prediction(x: int, db: Session = Depends(get_db)):
+    prediction = db.query(models.Predictions).filter(models.Predictions.x == x)
+
+    if prediction.first() is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"There is no prediction for x={x}")
+    
+    prediction.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/predict/{x}", response_model=schemas.Prediction)
+def update_prediction(x: int, updated_prediction: schemas.UpdatedPrediction, db: Session = Depends(get_db)):
+    update_query = db.query(models.Predictions).filter(models.Predictions.x == x)
+    prediction = update_query.first()
+    updated_prediction = updated_prediction.dict()
+    updated_prediction.update({"x": x})
+    if prediction == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"prediction with x: {x} does not exist")
+    update_query.update(updated_prediction, synchronize_session=False)
+    db.commit()
+    return update_query.first()
+
---
+++ NEW/fastapi/app/schemas.py
@@ -0,0 +1,56 @@
+from pydantic import BaseModel, RootModel, EmailStr
+from datetime import datetime
+from typing import List
+
+
+# Posts
+# ---------------------------------------------------------------
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+class PostCreate(PostBase):
+    pass    
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+# Users
+# ---------------------------------------------------------------
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+# Predictions
+# ---------------------------------------------------------------
+
+
+class PredictionBase(BaseModel):
+    x: float
+    y: float
+    
+class Prediction(PredictionBase):
+    pass
+
+class Feature(BaseModel):
+    x: list[float]
+
+class UpdatedPrediction(BaseModel):
+    y: float
+
+class PredictionsList(RootModel):
+    root: List[float]

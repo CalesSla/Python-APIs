a105a1ab48d1f64cb3a3e879bf3eb90546d4cd59
Slava Calestru <veaceslavcalestru@gmail.com>
2024-12-27 13:34:55 +0000

    set the basic alembic settings

--- NEW/fastapi/app/routers/post.py
+++ NEW/fastapi/app/routers/post.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from .. import models, schemas, oauth2
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from ..database import get_db
 from typing import List, Optional
 
 router = APIRouter(prefix="/posts", tags=["Posts"])
 
 
-@router.get("/", response_model=List[schemas.Post])
+@router.get("/", response_model=List[schemas.PostOut])
 def get_posts(db: Session = Depends(get_db), 
               current_user: int = Depends(oauth2.get_current_user),
               limit: int = 10,
@@ -17,11 +18,17 @@ def get_posts(db: Session = Depends(get_db),
     #                     FROM posts
     #                     """)
     # posts = cursor.fetchall()
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
-    return posts
+    # posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    results = (
+        db.query(models.Post, func.count(models.Vote.post_id).label("votes"))
+        .join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True)
+        .group_by(models.Post.id)
+        .filter(models.Post.title.contains(search)).limit(limit).offset(skip)
+        .all())
+    return results
 
  
-@router.get("/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.PostOut)
 def get_post(id: int, 
              db: Session = Depends(get_db), 
              current_user: int = Depends(oauth2.get_current_user)):
@@ -32,7 +39,11 @@ def get_post(id: int,
     #                 """, (str(id),))
     # post = cursor.fetchone()
 
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    post = db.query(models.Post)
+
+    post = (db.query(models.Post, func.count(models.Vote.post_id).label("votes"))
+            .join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True)
+            .group_by(models.Post.id).filter(models.Post.id == id).first())
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
--- NEW/fastapi/app/schemas.py
+++ NEW/fastapi/app/schemas.py
@@ -45,6 +45,14 @@ class Post(PostBase):
         orm_mode = True
 
 
+class PostOut(BaseModel):
+    Post: Post
+    votes: int
+
+    class Config:
+        orm_mode = True
+
+
 # Authentication
 # ---------------------------------------------------------------
 
@@ -70,7 +78,7 @@ class TokenData(BaseModel):
 class Vote(BaseModel):
     post_id: int
     dir: conint(le=1)
-    
+
 
 
 # Predictions
